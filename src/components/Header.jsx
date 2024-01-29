faa52ca1ba6636eacfaa8ef0608cc5e94061bc9a
giovgio90 <giovgio90@gmail.com>
2024-01-29 23:55:07 +0100

    Start project curved menu

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Nav from "./Nav";
+import { AnimatePresence } from "framer-motion";
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <>
+      <div>
+        <div
+          onClick={() => {
+            setIsActive(!isActive);
+          }}
+          className="button"
+        >
+          <div className={`burger ${isActive ? "burgerActive" : ""}`}></div>
+        </div>
+      </div>
+
+      <AnimatePresence mode="wait">{isActive && <Nav />}</AnimatePresence>
+    </>
+  );
+};
+
+export default Header;
